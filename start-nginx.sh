65030cbfe9d1a394edb2b806e8998e470c2bc377
Josh Rudnik <51795153+JSH32@users.noreply.github.com>
2024-07-23 20:27:10 +0000

    Typo, add export

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PORTAINER_HOSTNAME="${PORTAINER_HOSTNAME:-portainer}"
-PORTAINER_PORT="${PORTAINER_PORT:-9000}"
+export PORTAINER_HOSTNAME="${PORTAINER_HOSTNAME:-portainer}"
+export PORTAINER_PORT="${PORTAINER_PORT:-9000}"
 envsubst '$PORTAINER_HOSTNAME,$PORTAINER_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
